680cd9fb3c2193e5e92e1572e4f80c5d883592e5
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-09-18 23:26:15 +1000

    - formatting

--- app/MatchLifestyleFacility.Demo/FooInstaller.cs
+++ app/MatchLifestyleFacility.Demo/FooInstaller.cs
@@ -11,8 +11,7 @@ namespace ConsoleApplication1
 		{
 			container.AddFacility<MatchLifestyleFacility>();
 			container.Register(Component.For<IFoo>().MatchLifestyleTo("inner"),
-			                   Component.For<IFoo>().ImplementedBy<Foo>().Named("inner").LifeStyle.Transient
-				);
+			                   Component.For<IFoo>().ImplementedBy<Foo>().Named("inner").LifeStyle.Transient);
 		}
 	}
 }
